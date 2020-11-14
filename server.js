754af0aae03e7cab4c92141144c16b650f1eedf7
harshsri28 <66912920+harshsri28@users.noreply.github.com>
2020-11-14 12:15:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
+
+import Cards from './dbCards.js';
+import dbCards from "./dbCards.js";
+
+//App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://admin:AfBkfigzjkqQXwUO@cluster0.qj35l.mongodb.net/tinderdb?retryWrites=true&w=majority`
+
+//Middle-layers
+app.use(express.json());
+app.use(Cors());
+
+
+//DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+
+// API endpoints
+app.get("/", (req,res) => res.status(200).send("HELLO "));
+
+app.post("/tinder/cards", (req, res) => {
+    const dbCards = req.body; 
+
+    Cards.create(dbCards, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+
+app.get("/tinder/cards", (req,res) => {
+    Cards.find((err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }else {
+            res.status(200).send(data);
+        }
+    });  
+});
+
+// Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
